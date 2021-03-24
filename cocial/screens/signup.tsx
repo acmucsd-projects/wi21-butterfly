23674ceb4dc30c63bfcbf40d6e47110cf8c8963e
AnthonyM456 <anthonymanrique456@gmail.com>
2021-03-23 23:37:29 -0700

    Updated Signup Page (Needs further improvement - V1)

--- cocial/screens/signup.tsx
+++ cocial/screens/signup.tsx
@@ -1,19 +1,24 @@
 import React, { useState } from 'react';
-import { Button, StyleSheet, Text, View, TextInput } from 'react-native';
-import DatePicker from 'react-native-date-picker'
+import { Button, StyleSheet, Text, View, TextInput, Switch } from 'react-native';
+// import DatePicker from 'react-native-date-picker';
+import { colors } from '../assets/theme'
 
 export default function Signup() {
   const [value, setValue] = useState(false);
 
   const clickHandler = () => {
     if(!value){
-      alert('Successful Sign Up');
+      alert(name + " " + email + " " + password);
       setValue(true);
     }
     else
-    alert('Signed Up');
+    alert("Signed Up: " + name + " " + email + " " + password);
     
   }
+
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled(previousState =>
+     !previousState);
 
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
@@ -29,67 +34,117 @@ export default function Signup() {
     </View>
 
       <View style={styles.container}>
-        <TextInput
-          placeholder='name'
-          style={styles.input}
-          onChangeText={(val) => setName(val)} />
+        <View style={styles.inputContainers}>
+          <TextInput
+            placeholder='name'
+            style={styles.input}
+            onChangeText={(val) => setName(val)} />
 
-        <TextInput
-          textContentType='emailAddress'
-          placeholder='email'
-          keyboardType='email-address'
-          style={styles.input}
-          onChangeText={(val) => setEmail(val)} />
+          <TextInput
+            textContentType='emailAddress'
+            placeholder='email'
+            keyboardType='email-address'
+            style={styles.input}
+            onChangeText={(val) => setEmail(val)} />
 
-        <TextInput
-          textContentType='password'
-          placeholder='password'
-          secureTextEntry={true}
-          style={styles.input}
-          onChangeText={(val) => setPassword(val)} />
+          <TextInput
+            textContentType='password'
+            placeholder='password'
+            secureTextEntry={true}
+            style={styles.input}
+            onChangeText={(val) => setPassword(val)} />
 
-
-        <View style={styles.buttonContainer}>
-          <Button color = 'white' title='Sign Up' onPress={clickHandler} />
+          <TextInput
+            placeholder='birthday (MM/DD/YY)(CHANGE)'
+            style={styles.input}
+            onChangeText={(val) => setPassword(val)} />
         </View>
 
-        
+          <Text>College Dropdown (CHANGE)</Text>
 
-        <Text>
-          name: {name}, email: {email}, password: {password} college: {college} birthday: {birthday}
-        </Text>
+          <View style={styles.switchContainer}>
+            <Switch
+            trackColor={{ false: "#767577", true: "#99DE9F" }}
+            thumbColor={isEnabled ? "#f4f3f4" : "#f4f3f4"}
+            onValueChange={toggleSwitch}
+            value={isEnabled}
+            />
+            <Text style={styles.switchText}>I agree to the Terms and Services</Text>
+          </View>
+
+
+          <View style={styles.buttonContainer}>
+            <View style={styles.buttonText}>
+            <Button color = 'white' title='signup' onPress={clickHandler} />
+            </View>
+          </View>
+
       </View></>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginHorizontal: 50,
-    backgroundColor: '#FFFFFF',
-    // alignItems: 'center',
-    justifyContent: 'flex-start',
-  },
-  buttonContainer: {
-    backgroundColor: 'black',
-    marginHorizontal: 100,
-    marginVertical: 12,
-    borderRadius: 10,
-  },
   header: {
     height: 80,
     paddingTop: 50,
   },
   title: {
+    color: '#FEBB5B',
     textAlign: 'center',
     fontSize: 20,
     fontWeight: 'bold',
   },
+  container: {
+    flex: 1,
+    paddingHorizontal: "7%",
+    // marginHorizontal: 50,
+    backgroundColor: '#FFFFFF',
+    // alignItems: 'center',
+    justifyContent: 'space-evenly',
+  },
+  inputContainers: {
+    alignSelf: 'center',
+  },
   input: {
-    marginBottom: 10,
-    paddingHorizontal: 30,
-    paddingVertical: 6,
-    borderBottomWidth: 1,
+    marginBottom: 75,
+
+    paddingHorizontal: 2,
+    paddingVertical: 2,
+
+    fontSize: 18,
+    borderBottomWidth: 2,
     borderBottomColor: 'black',
+    color: 'gray',
+    width: 250,
  },
+  buttonContainer: {
+    backgroundColor: '#FEBB5B',
+    alignSelf: 'center',
+    width: "70%",
+    height: "7%",
+    // paddingVertical: 20,
+    borderRadius: 45,
+    // justifyContent: 'center',
+    
+    // 
+  },
+  switchContainer: {
+    flexDirection: "row",
+    alignSelf: 'center',
+    marginBottom: 10,
+    // justifyContent: 'flex-start',
+  },
+  switchText:{
+    paddingVertical: 7,
+    paddingHorizontal: 10,
+    fontWeight: 'bold'
+    // justifyContent: 'flex-end',
+    
+  },
+  buttonText:{
+    paddingVertical: 7,
+    paddingHorizontal: 10,
+    // justifyContent: 'flex-end',
+  },
+  
 });
