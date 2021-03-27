00c796177b08197649faa05e16ac84828d5f75e0
AnthonyM456 <anthonymanrique456@gmail.com>
2021-03-27 00:05:58 -0700

    Updated (almost) finalized Dashboard

--- client/App.tsx
+++ client/App.tsx
@@ -9,6 +9,9 @@ export default function App() {
   const [fontsLoaded] = useFonts ({
     'OpenSans': require('./assets/fonts/OpenSans-Regular.ttf'),
     'OpenSans-SemiBold' : require('./assets/fonts/OpenSans-SemiBold.ttf'),
+    'GlacialIndifference-Regular' : require('./assets/fonts/GlacialIndifference-Regular.ttf'),
+    'GlacialIndifference-Italic' : require('./assets/fonts/GlacialIndifference-Italic.ttf'),
+    'GlacialIndifference-Bold' : require('./assets/fonts/GlacialIndifference-Bold.ttf'),
   })
   if (fontsLoaded) {
     return (
